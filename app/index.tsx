df62a6372915fa1a2ebdacb7ed95819908dcd675
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-10-08 15:17:03 +0000

    ✨feat(app): Creacion del componente CustomButton -Implementacion de useState en index -Implementacion de mas estilos de texto en CustomText -Estilos de boton en CustomButton

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,22 @@
 import CustomText from "@/components/ui/CustomText";
 import "@/global.css";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
 export default function Index() {
+  const [isLogin, setIsLogin] = useState<string>("");
+  useEffect(() => {
+    switch (isLogin){
+      case "Login": 
+      Alert.alert("Login");
+      break;
+      case "Register":
+      break;
+    }
+    
+    return () => {
+      
+    };
+  }, []);
   return (
     <View className="bg-[#026A75] w-screen h-screen">
       <CustomText variant="large" dark>
--- components/ui/CustomText.tsx
+++ components/ui/CustomText.tsx
@@ -1,35 +1,54 @@
 import React from 'react';
 import { Text } from 'react-native';
-interface CustomTextProps{
-    variant:"small"|"medium"|"large";
-    dark?:boolean;
-    children: React.ReactNode;
+
+interface CustomTextProps {
+  children: React.ReactNode;
+  size?: 'large' | 'medium' | 'small' | 'button';
+  color?: 'black' | 'white' | 'gray'| 'blue';
+  className?: string;
+  bold?: boolean;
+  underline?: boolean;
 }
 
-const CustomText = ({variant, dark =false, children}:CustomTextProps) => {
+export const CustomText = ({ 
+  children, 
+  size = 'medium', 
+  color = 'black', 
+  className = '',
+  bold = false,
+  underline = false
+}: CustomTextProps) => {
+  const getSizeStyles = () => {
+    switch (size) {
+      case 'large':
+        return 'text-6xl';
+      case 'medium':
+        return 'text-xl';
+      case 'small':
+        return 'text-base';
+      case 'button':
+        return 'text-lg text-center';
+      default:
+        return 'text-base';
+    }
+  };
+  const getColorStyles = () => {
+    switch (color) {
+      case 'white':
+        return 'text-white';
+      case 'black':
+        return 'text-black';
+      case 'gray':
+        return 'text-gray-500';
+      case 'blue':
+        return 'text-blue-500';
+      default:
+        return 'text-black';
+    }
+  };
   return (
-     <Text className={styleSelector(variant,dark)}>
-        {children}
+    <Text className={`${getColorStyles()} ${getSizeStyles()} ${bold ? 'font-bold' : ''} ${underline ? 'underline' : ''} ${className}`}>
+      {children}
     </Text>
- )
-}
-
-function styleSelector(variant:any,dark:boolean){
-    let style = ""
-    if(dark == true){
-        style += "text-[#9F211F] "
-    }else{
-        style += "text-white "
-    }
-    
-    switch(variant){
-        case "small":
-            return style + "font-semibold text-sm" ;
-        case "medium":
-            return style + "font-semibold text-base" ;
-        case "large":
-            return style + " font-semibold text-xl" ;
-    }
-}
-
-export default CustomText
+  );
+};
